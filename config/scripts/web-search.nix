bfee8857cfe7ee08246dc86c61f94b497b3baf55
nandarsigma06 <nandarsigma06@gmail.com>
2025-02-21 13:04:53 +0700

    menambahkan pencarian home manager option di websearch

--- config/scripts/web-search.nix
+++ config/scripts/web-search.nix
@@ -6,6 +6,7 @@ pkgs.writeShellScriptBin "web-search" ''
   URLS=(
     ["🌎 Search"]="https://search.brave.com/search?q="
     ["❄️  Unstable Packages"]="https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query="
+    ["🏠 Manager Options"]="https://home-manager-options.extranix.com/"
     ["🎞️ YouTube"]="https://www.youtube.com/results?search_query="
     ["🦥 Arch Wiki"]="https://wiki.archlinux.org/title/"
     ["🐃 Gentoo Wiki"]="https://wiki.gentoo.org/index.php?title="
