73cb341354e43151a64b5fdf40fd8ec8b63a1d76
nandarsigma06 <nandarsigma06@gmail.com>
2025-04-13 05:30:59 +0700

    change config ncmpcpp music dir

--- config/home/ncmpcpp.nix
+++ config/home/ncmpcpp.nix
@@ -1,45 +1,50 @@
-{pkgs, ...}:{
-
-programs = {
-        ncmpcpp = {
-          enable = true;
-          bindings = [
-            { key = "j"; command = [ "scroll_down" ]; }
-            { key = "k"; command = [ "scroll_up" ]; }
-            { key = "J"; command = [ "select_item" "scroll_down" ]; }
-            { key = "K"; command = [ "select_item" "scroll_up" ]; }
-          ];
-          settings = {
-            mpd_host = "localhost";
-            mpd_music_dir = "/home/nandar/Music";
-            mpd_port = "6600";
-            autocenter_mode = "yes";
-            centered_cursor = "yes";
-            external_editor = "lvim";
-            header_visibility = "no";
-            ignore_leading_the = true;
-            main_window_color = "blue";
-            message_delay_time = 1;
-            playlist_disable_highlight_delay = 2;
-            progressbar_color = "black";
-            progressbar_elapsed_color = "blue";
-            progressbar_look = "━━━";
-            statusbar_visibility = "no";
-            titles_visibility = "no";
-            user_interface = "alternative";
-            visualizer_color = "blue, green";
-            visualizer_data_source = "/tmp/mpd.fifo";
-            visualizer_look = "●●";
-            visualizer_output_name = "mpd-visualizer";
-            visualizer_type = "ellipse";
-          };
-        };
+{pkgs, ...}: {
+  programs = {
+    ncmpcpp = {
+      enable = true;
+      bindings = [
+        {
+          key = "j";
+          command = ["scroll_down"];
+        }
+        {
+          key = "k";
+          command = ["scroll_up"];
+        }
+        {
+          key = "J";
+          command = ["select_item" "scroll_down"];
+        }
+        {
+          key = "K";
+          command = ["select_item" "scroll_up"];
+        }
+      ];
+      mpdMusicDir = "~/Music";
+      settings = {
+        mpd_host = "localhost";
+        # mpd_music_dir = "/home/nandar/Music";
+        mpd_port = "6600";
+        autocenter_mode = "yes";
+        centered_cursor = "yes";
+        external_editor = "lvim";
+        header_visibility = "no";
+        ignore_leading_the = true;
+        main_window_color = "blue";
+        message_delay_time = 1;
+        playlist_disable_highlight_delay = 2;
+        progressbar_color = "black";
+        progressbar_elapsed_color = "blue";
+        progressbar_look = "━━━";
+        statusbar_visibility = "no";
+        titles_visibility = "no";
+        user_interface = "alternative";
+        visualizer_color = "blue, green";
+        visualizer_data_source = "/tmp/mpd.fifo";
+        visualizer_look = "●●";
+        visualizer_output_name = "mpd-visualizer";
+        visualizer_type = "ellipse";
       };
-
-
-
-
-
-
-
+    };
+  };
 }
