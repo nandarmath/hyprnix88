9045173459ef8dc7dae672daff7e68aadc6b2a24
nandarsigma06 <nandarsigma06@gmail.com>
2025-06-15 11:28:46 +0700

    add: plugins markdown preview and quarto on nixvim

--- config/home/nxchad.nix
+++ config/home/nxchad.nix
@@ -1,17 +1,51 @@
-{pkgs, ...}:{
+{ pkgs, ... }:
+{
 
+  programs.nixvim = {
+    enable = true;
+    plugins.cmp-path.enable = true;
+    plugins.cmp-path.autoLoad = true;
+    keymaps = [
+      {
+        mode = "i";
+        key = "jk";
+        action = "<Esc>";
+      }
+    ];
+    plugins.markdown-preview = {
+      enable = true;
+      autoLoad = true;
+      settings = {
+        auto_close = 0;
+        auto_start = 0;
+        combine_preview = 1;
+        combine_preview_auto_refresh = 1;
+        preview_options = {
+          katex = { };
+          maid = { };
+          flowchart_diagrams = { };
+          toc = { };
+          sequence_diagrams = { };
+        };
+      };
+    };
 
-programs.nixvim = {
-  enable = true;
-  plugins.cmp-path.enable = true;
-  plugins.cmp-path.autoLoad = true;
+    plugins.quarto = {
+      enable = true;
+      autoLoad = true;
+      settings = {
+        codeRunner.enabled = true;
+        codeRunner.default_method = "molten";
+        lspFeatures.completion.enabled = true;
+        lspFeatures.chunks = "curly";
+        lspFeatures.languages = ["r" "python"];
+        lspFeatures.diagnostics.enabled = true;
+      };
+    };
   };
-programs.nixvim = {
-          # change theme nvchad
-  nvchad.config.base46.theme = "starlight";
+  programs.nixvim = {
+    # change theme nvchad
+    nvchad.config.base46.theme = "starlight";
   };
-
-
-
 
 }
--- flake.nix
+++ flake.nix
@@ -26,7 +26,8 @@
     };
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    nxchad.url = "github:nandarmath/nxchad";
+    # nxchad.url = "github:nandarmath/nxchad";
+    nxchad.url = "git:/home/nandar/Documents/NxChad";
     # This is important, since nxchad dosn't add nixpkgs repo in dependencies
     nxchad.inputs.nixpkgs.follows = "nixpkgs";
     # nvchad4nix = {
